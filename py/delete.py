7211b1549f41d2b854d5250cd797b0ada8ce1c62
Adarsh0911 <adarshsingh91913@gmail.com>
2025-09-07 21:36:34 +0530

    Delete py directory

--- py/delete.py
+++
@@ -1,9 +0,0 @@
-username_to_delete = 'Janvi'
-
-conn = sqlite3.connect('users.db')
-cur = conn.cursor()
-cur.execute("DELETE FROM users WHERE username=?", (username_to_delete,))
-conn.commit()
-conn.close()
-
-print(f"✅ User '{username_to_delete}' deleted.")
